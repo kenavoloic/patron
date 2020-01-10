e775760ce86356b629f286ccb3d50d61d511c819
Loic Kenavo <kenavoloic@gmail.com>
2020-01-10 01:33:26 +0100

    modif webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const source = path.join(__dirname, "./src");
 const distribution = path.join(__dirname, "./dist");
 
 module.exports = {
-    entry: path.join(source, "./src/index.js"),
+    entry: path.join(source, "./index.js"),
     output: {
         path: distribution,
         filename: "./cca.[hash].js"
@@ -13,7 +13,7 @@ module.exports = {
     mode: "development",
     module: {
         rules: [
-            {test: /\.js$?/, exclude: /node_modules/, use: ["babel-loader"]},
+            {test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"]},
             {test: /\.css$/, use: ["style-loader", "css-loader"]}
         ]
     },
